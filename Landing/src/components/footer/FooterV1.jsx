b073c0b60f8d0e3d967f521bf7729e2fc9ad4a96
Dhanuka Dilshan <dhanukatt03@gmail.com>
2025-09-12 22:27:18 +0530

    update

--- Landing/src/components/footer/FooterV1.jsx
+++ Landing/src/components/footer/FooterV1.jsx
@@ -45,7 +45,6 @@ const FooterV1 = () => {
                     src="/img/logo/logo-light.png"
                     alt="Logo"
                   />
-                  
                   <p>
                     Empowering individuals by providing seamless access to
                     expert advice across various fields, from personal growth to
